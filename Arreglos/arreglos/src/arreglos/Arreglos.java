8f0322535639e04c7b2d60d447bc8eecde236982
David <jhandry13@hotmail.com>
2018-05-31 10:17:29 -0500

    Ejercicios Arreglos

---
+++ Arreglos/arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author jhand
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] c = new int[4];
+        c[0] = 10;
+
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
+    }
+
+}
